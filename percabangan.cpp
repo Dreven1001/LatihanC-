c59205eec9967588e237a461148d8438cbb4d5b4
Dreven1001 <drevengameswikdo@gmail.com>
2025-09-17 13:40:25 +0700

    Update percabangan.cpp

--- percabangan.cpp
+++ percabangan.cpp
@@ -9,11 +9,12 @@ int main() {
     cin >> x;
 
     if (x > 500000) { // Jika belanja lebih dari 500.000
-        cout << "Selamat, anda dapat diskontol 20%" << endl;
+        cout << "Selamat, anda dapat diskon 20%" << endl;
     } else if (x > 200000) { // Jika belanja lebih dari 200.000
-        cout << "Selamat, anda dapat diskontol 10%" << endl;
+        cout << "Selamat, anda dapat diskon 10%" << endl;
     } else { // Jika belanja kurang dari 200.000
         cout << "Maaf, anda tidak dapat diskon" << endl;
     }
     return 0;
+
 }
