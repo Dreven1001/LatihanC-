f456817583913806fa31046dcabd1cd60c22adbd
Dreven1001 <drevengameswikdo@gmail.com>
2025-09-18 21:48:35 +0700

    Update tugas.cpp

--- tugas.cpp
+++ tugas.cpp
@@ -1,3 +1,6 @@
+// Tugas C++ Agung
+// Program gunting batu crt
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -5,9 +8,9 @@
 using namespace std;
 
 int main() {
-    
-    srand(time(0));
 
+    srand(time(0));
+    
     int x, ai;
     int playerscore = 0;
     int aiscore = 0;
@@ -16,62 +19,73 @@ int main() {
 
     ai = rand() % 5 + 1;
     cout << "- round 1 -" << endl;
-    cout << "player point: " << playerscore << endl;
-    cout << "ai point: " << aiscore << endl;
+    cout << "player score: " << playerscore << endl;
+    cout << "ai score: " << aiscore << endl;
     cout << "1. rock\n2. scissors\n3. lizard\n4. paper\n5. spock";
     cout << "\nplayer chooses: ";
     cin >> x;
     cout << "ai chooses: " << ai << endl;
-    if (x > ai) {
+    if (x == ai) {
+        cout << "it's draw!\n\n";
+    } else if ((x == 1 && (ai == 2 || ai == 3)) ||
+               (x == 2 && (ai == 3 || ai == 4)) ||
+               (x == 3 && (ai == 4 || ai == 5)) ||
+               (x == 4 && (ai == 5 || ai == 1)) ||
+               (x == 5 && (ai == 1 || ai == 2))) {
         cout << "player win!\n\n";
         playerscore++;
-    } else if (x < ai) {
+    } else {
         cout << "ai win!\n\n";
         aiscore++;
-    } else {
-        cout << "it's draw!\n\n";
     }
 
     ai = rand() % 5 + 1;
     cout << "- round 2 -" << endl;
-    cout << "player point: " << playerscore << endl;
-    cout << "ai point: " << aiscore << endl;
+    cout << "player score: " << playerscore << endl;
+    cout << "ai score: " << aiscore << endl;
     cout << "1. rock\n2. scissors\n3. lizard\n4. paper\n5. spock";
     cout << "\nplayer chooses: ";
     cin >> x;
     cout << "ai chooses: " << ai << endl;
-    if (x > ai) {
+    if (x == ai) {
+        cout << "it's draw!\n\n";
+    } else if ((x == 1 && (ai == 2 || ai == 3)) ||
+               (x == 2 && (ai == 3 || ai == 4)) ||
+               (x == 3 && (ai == 4 || ai == 5)) ||
+               (x == 4 && (ai == 5 || ai == 1)) ||
+               (x == 5 && (ai == 1 || ai == 2))) {
         cout << "player win!\n\n";
         playerscore++;
-    } else if (x < ai) {
+    } else {
         cout << "ai win!\n\n";
         aiscore++;
-    } else {
-        cout << "it's draw!\n\n";
     }
 
     ai = rand() % 5 + 1;
     cout << "- round 3 -" << endl;
-    cout << "player point: " << playerscore << endl;
-    cout << "ai point: " << aiscore << endl;
+    cout << "player score: " << playerscore << endl;
+    cout << "ai score: " << aiscore << endl;
     cout << "1. rock\n2. scissors\n3. lizard\n4. paper\n5. spock";
     cout << "\nplayer chooses: ";
     cin >> x;
     cout << "ai chooses: " << ai << endl;
-    if (x > ai) {
+    if (x == ai) {
+        cout << "it's draw!\n\n";
+    } else if ((x == 1 && (ai == 2 || ai == 3)) ||
+               (x == 2 && (ai == 3 || ai == 4)) ||
+               (x == 3 && (ai == 4 || ai == 5)) ||
+               (x == 4 && (ai == 5 || ai == 1)) ||
+               (x == 5 && (ai == 1 || ai == 2))) {
         cout << "player win!\n\n";
         playerscore++;
-    } else if (x < ai) {
+    } else {
         cout << "ai win!\n\n";
         aiscore++;
-    } else {
-        cout << "it's draw!\n\n";
     }
 
     cout << "=== final result ===" << endl;
     cout << "player score: " << playerscore << endl;
     cout << "ai score: " << aiscore << endl;
-
     if (playerscore > aiscore) {
         cout << ">> player menang! <<\n";
     } else if (aiscore > playerscore) {
@@ -79,6 +93,5 @@ int main() {
     } else {
         cout << ">> seri! <<\n";
     }
-
     return 0;
 }
